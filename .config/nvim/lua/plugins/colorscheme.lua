2b26516e4034fc7571089102fbd1c591a4dd99ac
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-02-18 12:45:00 -0500

    Change colorscheme to vscode

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,15 @@
--- Set the default colorscheme
 return {
+  -- Add external colorschemes
+  {
+    "Mofiqul/vscode.nvim",
+  },
+  -- Set the default colorscheme
+  -- Pass in the name of an external colorscheme that is defined above,
+  -- or use a built-in colorscheme. See colorschemes via <Leader>uC
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin", -- provided by default by LazyVim
+      colorscheme = "vscode",
     },
-  }
+  },
 }
