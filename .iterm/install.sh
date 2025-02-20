930959df18bb41d4f362764bdea404e914f466c6
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-02-20 11:39:03 -0500

    Add iterm2 configs

---
+++ .iterm/install.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.iterm"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
