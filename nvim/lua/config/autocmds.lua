3b43687d12f04d7ffc47acc8fc8725efe47e9369
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-03-18 13:32:56 -0400

    Add line length limit guides for git commit messages and markdowns

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,23 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-------------------------------------
+-- Add line length limit guides
+-------------------------------------
+
+-- Set colorcolumn=72 for git commit messages
+vim.api.nvim_create_autocmd("BufRead", {
+  pattern = "COMMIT_EDITMSG",
+  callback = function()
+    vim.opt_local.colorcolumn = "72"
+  end,
+})
+
+-- Set colorcolumn=80 for Markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.colorcolumn = "80"
+  end,
+})
