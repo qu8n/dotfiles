68d2ed8b8d278f2d170a7bc324e8585b2e07eec2
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-05-06 16:53:49 -0400

    Add keymap to accept word by word in Copilot suggestion

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      auto_trigger = true,
+      keymap = {
+        -- Accept word by word in the suggestion
+        -- Source: https://github.com/LazyVim/LazyVim/discussions/2109#discussioncomment-7738035
+        accept_word = "<C-l>",
+      },
+    },
+    filetypes = {
+      ["."] = true,
+    },
+  },
+}
