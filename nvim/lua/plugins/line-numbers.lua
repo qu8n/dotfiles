5a355c2fc0ff6648702652c87f3ee2034f5d7376
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-05-25 05:21:29 -0400

    Show both abs and rel line numbers and update nvim plugins

---
+++ nvim/lua/plugins/line-numbers.lua
@@ -0,0 +1,8 @@
+-- Display both absolute and relative line numbers
+return {
+  "shrynx/line-numbers.nvim",
+  opts = {
+    -- Set custom color for the absolute line numbers
+    abs_highlight = { fg = "#80A0FF" },
+  },
+}
