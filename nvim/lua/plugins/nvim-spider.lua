3147cb6a37c4dd7ef7b278125769e9ae6e9f7de9
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-05-31 08:15:30 -0700

    Install nvim-spider for better Vim word motions

---
+++ nvim/lua/plugins/nvim-spider.lua
@@ -0,0 +1,9 @@
+-- Better Vim word motions `w`, `e`, `b` that work with punctuation and special characters
+return {
+  "chrisgrieser/nvim-spider",
+  keys = {
+    { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+    { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+    { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+  },
+}
